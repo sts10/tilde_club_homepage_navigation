76d6ea29de4aeef79b425af77f5d9730b808580e
sts10 <sschlinkert@gmail.com>
2014-10-11 11:11:49 -0400

    knows whjat href its on

--- js/keypress.js
+++ js/keypress.js
@@ -5,9 +5,13 @@ $(document).ready(function(){
     // some code to direct browser to http://tilde.club/~ + tilde_member_name
   }
 
+  // http://jsfiddle.net/MKZSE/77/
+
+  var link_index = -1;
+
   // listens for any navigation keypress activity
   $(document).keypress(function(e){
-    alert('some key was pressed');
+    // alert('some key was pressed');
     if(e.which == 106){ // j 
       // alert("you pressed j");
     } 
@@ -18,6 +22,11 @@ $(document).ready(function(){
     if(e.keyCode==107){ // k
         Navigate(-1);
     }
+
+    if(e.keyCode==13){ // carriage return
+        alert("would open link_index of " + link_index);
+
+    }    
   });
 
   // http://jsfiddle.net/MKZSE/77/
@@ -33,6 +42,13 @@ $(document).ready(function(){
            link_index = link_list.length - 1;
       var cssClass = "selected_link";
       link_list.removeClass(cssClass).eq(link_index).addClass(cssClass);
+
+      var addressValue = $(".selected_link").attr("href");
+      alert(addressValue);
+  }
+
+  function makeLinkArray(){
+
   }
 
 
