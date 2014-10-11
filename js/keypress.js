ee67edf796ef2b5a1597276382f3115a8e23d94c
sts10 <sschlinkert@gmail.com>
2014-10-11 10:54:22 -0400

    got the navigate function working, plus added the selected_link css class to index.html

--- js/keypress.js
+++ js/keypress.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function(){
-  alert('hello');
  
   function goToKeyedURL(tilde_member_name){
     // some code to direct browser to http://tilde.club/~ + tilde_member_name
@@ -10,28 +9,30 @@ $(document).ready(function(){
   $(document).keypress(function(e){
     alert('some key was pressed');
     if(e.which == 106){ // j 
-      alert("you pressed j");
+      // alert("you pressed j");
     } 
 
-    if (e.keyCode == 40) {  // down?
+    if (e.keyCode == 106) {  // j
         Navigate(1);
     }
-    if(e.keyCode==38){ // up?
+    if(e.keyCode==107){ // k
         Navigate(-1);
     }
   });
 
   // http://jsfiddle.net/MKZSE/77/
 
+  var link_index = -1;
+
   var Navigate = function(diff) {
-      displayBoxIndex += diff;
-      var oBoxCollection = $(".display_box");
-      if (displayBoxIndex >= oBoxCollection.length)
-           displayBoxIndex = 0;
-      if (displayBoxIndex < 0)
-           displayBoxIndex = oBoxCollection.length - 1;
-      var cssClass = "display_box_hover";
-      oBoxCollection.removeClass(cssClass).eq(displayBoxIndex).addClass(cssClass);
+      link_index += diff;
+      var link_list = $("li a");
+      if (link_index >= link_list.length)
+           link_index = 0;
+      if (link_index < 0)
+           link_index = link_list.length - 1;
+      var cssClass = "selected_link";
+      link_list.removeClass(cssClass).eq(link_index).addClass(cssClass);
   }
 
 
